5d6c1db7ca4f0327de46c940c8dd7f24953a1d43
florian ricq <florian.ricq@gmail.com>
2022-09-15 13:53:52 +0200

    couverture details page movie

--- src/pages/DetailsMovie/index.jsx
+++ src/pages/DetailsMovie/index.jsx
@@ -2,7 +2,7 @@ import ApiMovie from '../../ApiMovie'
 import { useParams } from 'react-router-dom'
 import MovieSection from '../../components/MovieSection/MovieSection'
 import { useQuery } from 'react-query'
-import { Box, Heading, Image, Text } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading, Image, Text } from '@chakra-ui/react'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
 import TvShowHome from '../../components/TvShowHome/TvShowHome'
@@ -36,7 +36,67 @@ function DetailsMovie() {
   return (
     <Box>
       <Header />
-      <Box mt={100} className="fond-noir">
+      <Box mt={16} className="fond-noir">
+        <Box
+          backgroundPosition={'center'}
+          backgroundSize={'cover'}
+          h={'80vh'}
+          bgImage={{
+            base: `url(https://image.tmdb.org/t/p/original/${data.poster_path})`,
+            sm: `url(https://image.tmdb.org/t/p/original${data.backdrop_path})`,
+          }}
+        >
+          <Box
+            w="inherit"
+            h="inherit"
+            bgGradient="linear(to-t, #111 10%, transparent 70%)"
+          >
+            <Flex
+              flexDirection={'column'}
+              justifyContent={'flex-end'}
+              pl={30}
+              pb={150}
+              pt={70}
+              w="inherit"
+              h="inherit"
+              //bgGradient="linear(to-r, #111 10%, transparent 70%)"
+            >
+              <Heading
+                fontWeight={'bold'}
+                fontSize={{ base: 25, md: 35, lg: 70 }}
+              >
+                {data.title}
+              </Heading>
+              <Flex mt={15}>
+                <Button
+                  fontSize={{ base: 16, md: 16, lg: 20 }}
+                  fontWeight="bold"
+                  borderRadius={5}
+                  mr={11}
+                  bg="#fff"
+                  color="#000"
+                  _hover={{
+                    opacity: '0.7',
+                  }}
+                >
+                  Lecture
+                </Button>
+                <Button
+                  _hover={{
+                    opacity: '0.7',
+                  }}
+                  fontSize={{ base: 16, md: 16, lg: 20 }}
+                  fontWeight="bold"
+                  borderRadius={5}
+                  bg="#333"
+                  color="#fff"
+                >
+                  + Ma Liste
+                </Button>
+              </Flex>
+            </Flex>
+          </Box>
+        </Box>
         <Text>MOVIE</Text>
         <Text>{data.title}</Text>
         <Text>{data.overview}</Text>
